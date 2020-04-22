93a8c3ae2f69c3fc52bef12c48dc12cc0fa5593b
Tiger <917428360@qq.com>
2020-04-22 17:23:16 +0800

    Update influxdb.groovy

--- src/org/devops/influxdb.groovy
+++ src/org/devops/influxdb.groovy
@@ -3,7 +3,7 @@ package org.devops
 //创建数据库
 def CreateDb(dbName){
    sh """
-      curl -XPOST 'http://192.168.1.200:30086/query' --data-urlencode 'q=CREATE DATABASE \"${dbName}\"'"
+      curl -XPOST 'http://127.0.0.1:8086/query' --data-urlencode 'q=CREATE DATABASE \"${dbName}\"'"
       """
 }
 
@@ -20,7 +20,7 @@ def WriteData(){
     
     httpRequest httpMode: 'POST', 
             requestBody: """${measurementName},build_number=${BUILD_ID},build_agent_name=${BUILD_AGENT} project_name=\"${SERVICE_NAME}\",build_id=${BUILD_ID},build_time=${currentBuild.duration},build_result=${BUILD_RESULT},build_desc=\"${currentBuild.description}\",tests_passed=10,tests_failed=2,tests_skipped=3 1434055564000000000""", 
-            url: 'http://192.168.1.200:30086/write?db=jenkins'
+            url: 'http://127.0.0.1:8086/write?db=jenkins'
     
     
 }
